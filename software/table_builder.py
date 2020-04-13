ad13a5a091fa4c7efa13775e0c5e4a5971d51141
Rich Holmes <rs.holmes@gmail.com>
2020-04-13 14:13:15 -0400

    Add generator and period scales

--- software/table_builder.py
+++ software/table_builder.py
@@ -7,6 +7,25 @@
 # * table_builder.py from https://github.com/bpcmusic/telex_scales
 #
 # * Removed frequency table, added PROGMEM to declarations
+#
+# * Added capability to read files with format similar to Scala, but instead
+# * of scale data:
+#
+# * g <number>
+# * p <number>
+#
+# which means to generate a scale with the given number of notes using
+# g and period p. E.g.:
+#
+# ! pyth_12_gp.scl
+# !
+# 12-tone Pythagorean scale                                                     
+# 12
+# !
+# g 3/2
+# p 2/1
+#
+# If the p line is missing, 2/1 is assumed.
 
 import sys, getopt, math
 
@@ -25,7 +44,9 @@ def readFile(inputfile, scalenum, outputhandle, notecounter):
     description = ""
     values = 0
     scale = []
-
+    generator = 0.0
+    period = 2.0
+    
     with open(inputfile) as f:
         for line in f:
 	    line = line.strip()
@@ -37,16 +58,42 @@ def readFile(inputfile, scalenum, outputhandle, notecounter):
 	    if description == "":
 		description = line
 	    elif values == 0:
-		values = line
-	    elif "/" in line:
-		ratio = line.split("/")
-		value = float(ratio[0]) / float(ratio[1])
-		value = math.log(value) * 1200 / math.log(2)
-		scale.append(value)
-	    else:
-		scale.append(float(line))
-                
+                values = int(line)
+            else:
+                if line.startswith("g"):
+                    line = line[1:]
+                    if "/" in line:
+		        ratio = line.split("/")
+		        generator = float(ratio[0]) / float(ratio[1])
+		        generator = math.log(generator) * 1200 / math.log(2)
+                    else:
+                        generator = float(line)
+                elif line.startswith("p"):
+                    line = line[1:]
+                    if "/" in line:
+		        ratio = line.split("/")
+		        period = float(ratio[0]) / float(ratio[1])
+		        period = math.log(period) * 1200 / math.log(2)
+                    else:
+                        period = float(line)
+	        elif "/" in line:
+		    ratio = line.split("/")
+		    value = float(ratio[0]) / float(ratio[1])
+		    value = math.log(value) * 1200 / math.log(2)
+		    scale.append(value)
+	        else:
+		    scale.append(float(line))
 
+    if not (generator == 0):
+        value = generator
+        scale = [1200.0]
+        while len(scale) < values:
+            while value > period:
+                value -= period
+            scale.append (value)
+            value += generator
+        scale.sort()
+        
     table = []
 
     # assume that 0v is C0 and loop over the scale until we have exceeded DACTOP
@@ -90,8 +137,6 @@ def readFile(inputfile, scalenum, outputhandle, notecounter):
 
 # MAIN
 
-print "main"
-
 argv = sys.argv[1:]
 
 try:
@@ -107,8 +152,6 @@ notecounter = []
 for opt, arg in opts:
     if opt in ("-i", "--ifile"):
         inputfile = arg
-
-print inputfile;
 
 if inputfile == "":
     sys.exit(2)
